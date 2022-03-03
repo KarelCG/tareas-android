a64b3e4bd97022915f3a5ac5112c8e82f34893b6
Karel Castelan <karelcastelan.2000@gmail.com>
2022-03-03 01:16:05 -0600

    fix files

---
+++ Evidencia1/app/src/main/java/com/example/evidencia1/formulario_de_registro.java
@@ -0,0 +1,14 @@
+package com.example.evidencia1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class formulario_de_registro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario_de_registro);
+    }
+}
